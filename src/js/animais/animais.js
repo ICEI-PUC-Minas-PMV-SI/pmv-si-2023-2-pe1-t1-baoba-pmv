c4bf103c9f69c4fc89e304da41d29d782257592a
YMateus Valadares de Oliveira Freitas <mateus.freitas.1492336@sga.pucminas.br>
2023-11-26 20:35:13 -0300

    adicionada a parte dinamica da pagina de animais, o scroll da pagina de ecoturismo e o css da pagina de conteudo de animais

--- src/js/animais/animais.js
+++ src/js/animais/animais.js
@@ -26,7 +26,7 @@ async function setAnimals(biome, elementId) {
 
 		for (let i = 0; i < animals.length; i++) {
 			htmlContent += `
-        <a href="${biome}/${animals[i].id}" class="animais-extincao-tundra">
+        <a href="/src/pages/conteudo-animais.html?biome=${biome}&animal=${animals[i].id}" class="animais-extincao-tundra">
             <img
               width="382"
               height="240"
@@ -41,6 +41,56 @@ async function setAnimals(biome, elementId) {
 		}
 
 		document.querySelector(`#${elementId}`).innerHTML = htmlContent;
+	} catch (error) {
+		throw new Error(error);
+	}
+}
+
+async function getAnimalContext() {
+	const urlParams = new URLSearchParams(window.location.search);
+	const animal = urlParams.get('animal');
+	try {
+		const res = await fetch(`http://localhost:3000/animals/${animal}`);
+		const data = await res.json();
+		console.log(data);
+		document.querySelector(`#nome-animal`).textContent = data.name;
+		document.querySelector(`#imagem-animal`).src = data.img_url;
+		document.querySelector(`#imagem-animal`).alt = data.name;
+		document.querySelector(`#descricao-animal`).textContent = data.about;
+	} catch (error) {
+		throw new Error(error);
+	}
+}
+
+async function setOthersAnimals() {
+	try {
+		const urlParams = new URLSearchParams(window.location.search);
+		const biome = urlParams.get('biome');
+		const animals = await getAnimalsByBiome(biome);
+		const animal = urlParams.get('animal');
+
+		let htmlContent = ``;
+
+		for (let i = 0; i < animals.length; i++) {
+			if (animal == animals[i].id) {
+				continue;
+			}
+			htmlContent += `
+        <a href="/src/pages/conteudo-animais.html?biome=${biome}&animal=${animals[i].id}" class="animais-extincao-tundra">
+            <img
+              width="382"
+              height="240"
+              src="${animals[i].img_url}"
+              alt="${animals[i].name}"
+            />
+            <div class="card-opaco-tundra">
+              <p>${animals[i].name}</p>
+            </div>
+          </a>
+        `;
+		}
+
+		document.querySelector(`#container-outros-animais`).innerHTML = htmlContent;
 	} catch (error) {
 		throw new Error(error);
 	}
