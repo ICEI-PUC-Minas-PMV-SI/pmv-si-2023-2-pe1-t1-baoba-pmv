dcf90c0bf5d7ad54f60898a0623c8b73735c0c52
Nicolas Pontes <nicolas.pontes@nibo.com.br>
2023-11-26 14:40:57 -0300

    add maps api and component

--- src/js/ecotourism/destinos.js
+++ src/js/ecotourism/destinos.js
@@ -33,64 +33,65 @@ async function setDestinyData() {
       </div>
         `;
 
-        sessionStorage.setItem("lat", destinyData.lat);
-        sessionStorage.setItem("lon", destinyData.lon);
+		sessionStorage.setItem("lat", destinyData.lat);
+		sessionStorage.setItem("lon", destinyData.lon);
 
 		document.querySelector("#destiny-content").innerHTML = htmlContent;
-        await setWheatherData();
+		await setWheatherData();
+        await setMap(destinyData.lon, destinyData.lat);
 	} catch (error) {
 		throw new Error(error);
 	}
-
 }
 
 async function getWheatherData() {
-    try {
-        const lat = sessionStorage.getItem("lat");
-        const lon = sessionStorage.getItem("lon");
-        const api_key = "1ab81d8a84eeb81fede78f63c9376740";
-        const data = await fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${api_key}&units=metric&lang=pt_br`);
-        const weatherData = await data.json();
+	try {
+		const lat = sessionStorage.getItem("lat");
+		const lon = sessionStorage.getItem("lon");
+		const api_key = "1ab81d8a84eeb81fede78f63c9376740";
+		const data = await fetch(
+			`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${api_key}&units=metric&lang=pt_br`
+		);
+		const weatherData = await data.json();
 
-        console.log(weatherData);
-        return weatherData;
-    } catch (error) {
-        throw new Error(error);
-    }
+		console.log(weatherData);
+		return weatherData;
+	} catch (error) {
+		throw new Error(error);
+	}
 }
 
 class WheatherData {
-    constructor(temp, feels_like, pressure, humidity, description) {
-        this.temp = temp;
-        this.feels_like = feels_like;
-        this.pressure = pressure;
-        this.humidity = humidity;
-        this.description = description;
-    }
+	constructor(temp, feels_like, pressure, humidity, description) {
+		this.temp = temp;
+		this.feels_like = feels_like;
+		this.pressure = pressure;
+		this.humidity = humidity;
+		this.description = description;
+	}
 }
 
 async function treatsWheatherData() {
-    try {
-        const weatherData = await getWheatherData();
-        const wheather = new WheatherData(
-            weatherData.main.temp,
-            weatherData.main.feels_like,
-            weatherData.main.pressure,
-            weatherData.main.humidity,
-            weatherData.weather[0].description
-        );
+	try {
+		const weatherData = await getWheatherData();
+		const wheather = new WheatherData(
+			weatherData.main.temp,
+			weatherData.main.feels_like,
+			weatherData.main.pressure,
+			weatherData.main.humidity,
+			weatherData.weather[0].description
+		);
 
-        return wheather;
-
-    } catch (error) {
-        throw new Error(error);
-    }
+		return wheather;
+	} catch (error) {
+		throw new Error(error);
+	}
 }
 
 async function setWheatherData() {
-    try {
-        const wheather = await treatsWheatherData();
-        const htmlContent = `
+	try {
+		const wheather = await treatsWheatherData();
+		const htmlContent = `
         <div class="wheather-data">
             <div class="wheather-data-item">
                 <h4>Temperatura</h4>
@@ -115,9 +116,24 @@ async function setWheatherData() {
         </div>
         `;
 
-        document.querySelector("#wheather-container").innerHTML = htmlContent;
-    } catch (error) {
-        throw new Error(error);
-    }
+		document.querySelector("#wheather-container").innerHTML = htmlContent;
+	} catch (error) {
+		throw new Error(error);
+	}
 }
 
+async function setMap(lon, lat) {
+	const map = new ol.Map({ target: "map" });
+	const basemapId = "arcgis/streets";
+
+	map.setView(
+		new ol.View({
+			center: ol.proj.fromLonLat([lon, lat]),
+			zoom: 8,
+		})
+	);
+
+	const basemapURL = `https://basemapstyles-api.arcgis.com/arcgis/rest/services/styles/v2/styles/${basemapId}?token=AAPK87a8a8cc73464aa0a531a52324a471c5NsY34aPbTabfurVcAfKWHLVgjtNHb5z9OOIbjzKupy827Yjy65WYj5VKusTrWmLs`;
+
+	olms.apply(map, basemapURL);
+}
